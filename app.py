d0a67843ccebf0f99abb85b9a1715c5d0d19401a
advaithnaidu <advaith.naidu@ascte.org>
2025-04-22 13:46:03 -0500

    Post works refreshing
    Unable to post without refreshing

--- app.py
+++ app.py
@@ -1,17 +1,29 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 import json
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template('index.html')
 
-@app.route("/api/posts")
+@app.route('/api/posts')
 def git_post():
     with open('posts.json', 'r') as file:
         posts = json.load(file)
     return jsonify(posts)
 
+@app.route('/api/add_post', methods=['POST'])
+def add_post():
+    new_post = request.get_json()
+    
+    with open('posts.json', 'r') as file:
+        posts = json.load(file)
+    posts.insert(0, new_post)
+
+    with open('posts.json', 'w') as file:
+        json.dump(posts, file, indent=4)
+
+        return jsonify ({"status":"success"}), 201
 if __name__ == '__main__':
     app.run(debug=True)
